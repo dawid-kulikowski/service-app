6aceadcb17f1cc671a5053224d738ae3efb6abea
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-08-30 22:37:20 +0200

    Update

--- src/main/java/org/service/api/client/ClientDTO.java
+++ src/main/java/org/service/api/client/ClientDTO.java
@@ -10,11 +10,54 @@ public class ClientDTO {
     private String secondPhoneNumber;
     private String email;
 
+    public ClientDTO() {
+    }
+
     public ClientDTO(String name, String surname, String phoneNumber, String secondPhoneNumber, String email) {
         this.name = name;
         this.surname = surname;
         this.phoneNumber = phoneNumber;
         this.secondPhoneNumber = secondPhoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSecondPhoneNumber() {
+        return secondPhoneNumber;
+    }
+
+    public void setSecondPhoneNumber(String secondPhoneNumber) {
+        this.secondPhoneNumber = secondPhoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 
